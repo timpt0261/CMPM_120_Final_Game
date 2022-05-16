74303440a16c75ee2acb31d1a791d1081becea16
timpt0261 <rchavez0215@gmail.com>
2022-05-16 01:43:16 -0700

    figuing out how to get enmy yo move away from player

--- src/prefabs/Players/Enemy_ball.js
+++ src/prefabs/Players/Enemy_ball.js
@@ -1,7 +1,7 @@
 // Create Enemy Ball
 class Enemy_Ball extends Phaser.Physics.Arcade.Sprite {
-  constructor(scene, x, y, texture,size,frame) {
-    super(scene, x, y, texture, size,frame);
+  constructor(scene, x, y, texture, size, frame) {
+    super(scene, x, y, texture, size, frame);
     this.scene = scene;
     this.size = size;
 
@@ -10,18 +10,22 @@ class Enemy_Ball extends Phaser.Physics.Arcade.Sprite {
     scene.add.existing(this);
     scene.physics.add.existing(this);
     // Maintains collides equal body
-    this.body.setSize(170,170);
+    this.body.setSize(170, 170);
     this.body.setCircle(85);
     this.setScale(size / 85);
   }
 
   update(player) {
     // calculates distance between player and enemy
-    var dist = Phaser.Math.Distance.BetweenPoints(player, this);
-    // if dist is 500 or less player will move towards player
-    if (dist <= 500) {
+    let dist = Phaser.Math.Distance.BetweenPoints(player, this);
+    let sizeDiff = this.size - player.size;
+    console.log("enemy size : " + this.size + " Player size: " + player.size + " Diff: " + sizeDiff);
+    // if dist is 400 or less player will move towards player
+    if (dist <= 400 && sizeDiff >= 0) {
       console.log(dist);
-      this.scene.physics.moveToObject(this, player, 2000/this.size);
+      this.scene.physics.moveToObject(this, player, 2000 / this.size);
+    } else if (dist <= 200 && sizeDiff < 0) {
+      this.scene.physics.moveToObject(this, player, -2000 / this.size);
     }
   }
 
