5b63b22e8af17347fc76dc88c52de7d726a94303
timpt0261 <rchavez0215@gmail.com>
2022-05-28 23:08:51 -0700

    adding rest of code to first three levels

--- src/scenes/Levels/Level_01.js
+++ src/scenes/Levels/Level_01.js
@@ -24,7 +24,7 @@ class Level_01 extends Phaser.Scene {
         this.player = new Player(this, this.game.config.width / 2 , this.game.config.height / 2,'player',70, 10000,1).setOrigin(0.5,0.5);  //Origin default is (0.5,0.5)
 
         // Add enemies
-        this.createEnemies(2);
+        this.createEnemies();
 
 
         this.physics.add.collider(this.player, this.enemyGroup, EatOrDie, null, this);
@@ -55,17 +55,17 @@ class Level_01 extends Phaser.Scene {
 
     createEnemies(){
         this.enemyGroup = this.physics.add.group();
-        Phaser.Math.Between(spawnSizeMin, spawnSizeMax);
-        
+        let spawn_x =  Phaser.Math.Between(50,game.config.width - 100)
+        let spawn_y = Phaser.Math.Between(50, game.config.height - 100);
 
-        this.enemy_1 = new Enemy_Ball(this, Phaser.Math.Between(50,spawn_x), Phaser.Math.Between(50, spawn_y), "enemy", 50, 0).setOrigin(
+        this.enemy_1 = new Enemy_Ball(this,spawn_x ,spawn_y , "enemy", 50, 0).setOrigin(
             0.5,
             0.5
         );
 
         this.enemyGroup.add(this.enemy_1);
 
-        this.enemy_2 = new Enemy_Ball(this, Phaser.Math.Between(50, game.config.width - 100), Phaser.Math.Between(50, game.config.height - 100), "enemy", 80, 0).setOrigin(
+        this.enemy_2 = new Enemy_Ball(this,spawn_x, spawn_y, "enemy", 80, 0).setOrigin(
             0.5,
             0.5
         );
--- src/scenes/Levels/Level_03.js
+++ src/scenes/Levels/Level_03.js
@@ -3,7 +3,7 @@ class Level_03 extends Phaser.Scene {
     super("Level_03");
   }
 
-  create() {
+  create(){
     currentScene = 3;
     // set up Phaser-provided cursor key input
     cursors = this.input.keyboard.createCursorKeys();
@@ -11,53 +11,76 @@ class Level_03 extends Phaser.Scene {
     // set up Scene switcher
     this.input.keyboard.on("keydown", sceneSwitcher);
 
-    this.add.tileSprite(
-        0,
-        0,
-        game.config.height,
-        game.config.width,
-        "green_background"
-      )
-      .setOrigin(0, 0)
-      .setScale(2);
+    // Add background color
+    this.add.tileSprite(0, 0, game.config.height, game.config.width, "green_background")
+        .setOrigin(0, 0)
+        .setScale(2);
 
     // Add player
-    this.player = new Player(
-      this,
-      this.game.config.width / 2,
-      this.game.config.height / 2,
-      "player",
-      100,
-      10000,
-      1
-    ).setOrigin(0.5, 0.5); //Origin default is (0.5,0.5)
+    this.player = new Player(this, this.game.config.width / 2 , this.game.config.height / 2,'player',70, 10000,1).setOrigin(0.5,0.5);  //Origin default is (0.5,0.5)
 
-    // Add enemy
-    this.enemy_1 = new Enemy_Ball(this, 300, 300, "enemy", 20, 0).setOrigin(
-      0.5,
-      0.5
-    );
+    // Add enemies
+    this.createEnemies();
 
-    this.physics.add.overlap(
-      this.player,
-      this.enemy_1,
-      function (enemyEatsPlayer) {
-        enemyEatsPlayer.body.stop;
-        this.physics.world.removeCollider(collider);
-      },
-      null,
-      this
-    );
+
+    this.physics.add.collider(this.player, this.enemyGroup, EatOrDie, null, this);
+
 
     this.input.on('pointermove', (pointer) => {
-      this.mouse = pointer;
+        this.mouse = pointer;
     })
-  }
 
-  update() {
+    this.input.on('pointerdown', (pointer) =>{
+        mode == 0 ? mode = 1 : mode = 0;
+        mode == 0 ? console.log("In Grow Mode\n") : console.log("In Shrink Mode\n");
+        
+    });
+
+    this.bonk = this.sound.add("wallBonk");
+    this.eat = this.sound.add("eatEnemy");
+    this.getEaten = this.sound.add("getEaten");
+}
+
+update(){
     this.player.update(this.mouse);
+
+    this.physics.world.wrap(this.enemyGroup, 32);
     this.enemy_1.update(this.player);
-  }
+    this.enemy_2.update(this.player);
+}
+
+createEnemies(){
+  this.enemyGroup = this.physics.add.group();
+  let spawn_x =  Phaser.Math.Between(50,game.config.width - 100)
+  let spawn_y = Phaser.Math.Between(50, game.config.height - 100);
+
+  this.enemy_1 = new Enemy_Ball(this,spawn_x ,spawn_y , "enemy", 50, 0).setOrigin(
+      0.5,
+      0.5
+  );
+
+  this.enemyGroup.add(this.enemy_1);
+
+  this.enemy_2 = new Enemy_Ball(this,spawn_x, spawn_y, "enemy", 80, 0).setOrigin(
+      0.5,
+      0.5
+  );
+
+  this.enemyGroup.add(this.enemy_2);
+
+}
+
+createObstacles(){
+    //create door
+    this.redDoor = new Door(this, 60, 60, "redDoor", 0).setOrigin(0,0);
+    //create button
+    this.redButton = new Button(this, 40, 40, "redButton", 0, this.redDoor).setOrigin(0, 0);
+
+    //link the button and door so they can pass signals 
+
+    //create collision group between the button and player
+        //callback function that calls the makes the button make the door disappear 
+}
 
   // createAnimation(){}
 }
